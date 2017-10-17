64899855687fe9ef8a5d939937d5e51aa8e7adcb
Kelvin Jin <kelvinjin@google.com>
2017-10-17 12:04:44 -0700

    convert test file to es6

--- index.test.ts
+++ index.test.ts
@@ -1,208 +1,212 @@
-'use strict'
+import * as assert from 'assert';
+import * as extend from 'extend';
+import * as proxyquire from 'proxyquire';
 
-var assert = require('assert')
-var extend = require('extend')
-var proxyquire = require('proxyquire')
+// for types
+import * as _gcpMetadata from '.';
+type GcpMetadata = typeof _gcpMetadata;
+type RetryRequest = (reqOpts: Object, opts: Object,
+    callback: (err?: any, request?: any) => void) => void;
 
-var VALID_RESPONSE = {
+// tslint:disable-next-line:no-empty
+const noop = () => {};
+
+const VALID_RESPONSE = {
   statusCode: 200
-}
+};
 
-describe('gcpMetadata', function () {
-  var cachedGcpMetadata
-  var gcpMetadata
+describe('gcpMetadata', () => {
+  let cachedGcpMetadata: GcpMetadata;
+  let gcpMetadata: GcpMetadata;
 
-  var retryRequestOverride
-  var fakeRetryRequest = function () {
-    return (retryRequestOverride || function () {}).apply(null, arguments)
-  }
+  let retryRequestOverride: RetryRequest | null;
+  let fakeRetryRequest = (...args: any[]) => {
+    return ((retryRequestOverride || noop) as Function)(...args);
+  };
 
-  before(function () {
+  before(() => {
     cachedGcpMetadata = proxyquire('./index.js', {
       'retry-request': fakeRetryRequest
-    })
-  })
+    });
+  });
 
-  beforeEach(function () {
-    retryRequestOverride = null
+  beforeEach(() => {
+    retryRequestOverride = null;
     gcpMetadata = proxyquire('./index.js', {
       'retry-request': fakeRetryRequest
-    })
-    extend(gcpMetadata, cachedGcpMetadata)
-  })
+    });
+    extend(gcpMetadata, cachedGcpMetadata);
+  });
 
-  it('should create the correct accessors', function () {
-    assert.strictEqual(typeof gcpMetadata.instance, 'function')
-    assert.strictEqual(typeof gcpMetadata.project, 'function')
-  })
+  it('should create the correct accessors', () => {
+    assert.strictEqual(typeof gcpMetadata.instance, 'function');
+    assert.strictEqual(typeof gcpMetadata.project, 'function');
+  });
 
-  it('should access all the metadata properly', function (done) {
-    var BASE_URL = 'http://metadata.google.internal/computeMetadata/v1'
-    var TYPE = 'type'
+  it('should access all the metadata properly', (done) => {
+    const BASE_URL = 'http://metadata.google.internal/computeMetadata/v1';
+    const TYPE = 'type';
 
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       assert.deepEqual(reqOpts, {
         uri: BASE_URL + '/' + TYPE,
         headers: {
           'Metadata-Flavor': 'Google'
         }
-      })
+      });
+      callback(null, VALID_RESPONSE);
+    };
 
-      assert.strictEqual(opts.noResponseRetries, 0)
+    getMetadata(done);
+  });
 
-      callback(null, VALID_RESPONSE)
-    }
+  it('should access a specific metadata property', (done) => {
+    const BASE_URL = 'http://metadata.google.internal/computeMetadata/v1';
+    const TYPE = 'type';
+    const PROPERTY = 'property';
 
-    getMetadata(done)
-  })
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-  it('should access a specific metadata property', function (done) {
-    var BASE_URL = 'http://metadata.google.internal/computeMetadata/v1'
-    var TYPE = 'type'
-    var PROPERTY = 'property'
-
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
-
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       assert.deepEqual(reqOpts, {
         uri: BASE_URL + '/' + TYPE + '/' + PROPERTY,
         headers: {
           'Metadata-Flavor': 'Google'
         }
-      })
-      callback(null, VALID_RESPONSE)
-    }
+      });
+      callback(null, VALID_RESPONSE);
+    };
 
-    getMetadata(PROPERTY, done)
-  })
+    getMetadata(PROPERTY, done);
+  });
 
-  it('should accept an object with property and query fields', function (done) {
-    var BASE_URL = 'http://metadata.google.internal/computeMetadata/v1'
-    var TYPE = 'type'
-    var PROPERTY = 'property'
-    var QUERY = {
+  it('should accept an object with property and query fields', (done) => {
+    const BASE_URL = 'http://metadata.google.internal/computeMetadata/v1';
+    const TYPE = 'type';
+    const PROPERTY = 'property';
+    const QUERY = {
       key: 'value'
-    }
+    };
 
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       assert.deepEqual(reqOpts, {
         uri: BASE_URL + '/' + TYPE + '/' + PROPERTY,
         headers: {
           'Metadata-Flavor': 'Google'
         },
         qs: QUERY
-      })
-      callback(null, VALID_RESPONSE)
-    }
+      });
+      callback(null, VALID_RESPONSE);
+    };
 
     getMetadata({
       property: PROPERTY,
       qs: QUERY
-    }, done)
-  })
+    }, done);
+  });
 
-  it('should extend the request options', function (done) {
-    var BASE_URL = 'http://metadata.google.internal/computeMetadata/v1'
-    var TYPE = 'type'
-    var PROPERTY = 'property'
+  it('should extend the request options', (done) => {
+    const BASE_URL = 'http://metadata.google.internal/computeMetadata/v1';
+    const TYPE = 'type';
+    const PROPERTY = 'property';
 
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       assert.deepEqual(reqOpts, {
         uri: BASE_URL + '/' + TYPE + '/' + PROPERTY,
         headers: {
           'Metadata-Flavor': 'Google',
           'Custom-Header': 'Custom'
         }
-      })
-      callback(null, VALID_RESPONSE)
-    }
+      });
+      callback(null, VALID_RESPONSE);
+    };
 
-    var options = {
+    const options = {
       property: PROPERTY,
       headers: {
         'Custom-Header': 'Custom'
       }
-    }
+    };
 
-    var originalOptions = extend(true, {}, options)
+    const originalOptions = extend(true, {}, options);
 
-    getMetadata(options, function (err) {
-      assert.ifError(err)
-      assert.deepEqual(options, originalOptions) // wasn't modified
-      done()
-    })
-  })
+    getMetadata(options, (err) => {
+      assert.ifError(err);
+      assert.deepEqual(options, originalOptions); // wasn't modified
+      done();
+    });
+  });
 
-  it('should return the request error', function (done) {
-    var ERROR = 'fake error'
-    var TYPE = 'type'
+  it('should return the request error', (done) => {
+    const ERROR = 'fake error';
+    const TYPE = 'type';
 
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
-      callback(ERROR)
-    }
+    retryRequestOverride = (reqOpts, opts, callback) => {
+      callback(ERROR);
+    };
 
-    getMetadata(function (err) {
-      assert.strictEqual(err, ERROR)
-      done()
-    })
-  })
+    getMetadata((err) => {
+      assert.strictEqual(err, ERROR);
+      done();
+    });
+  });
 
-  it('should return error when res is empty', function (done) {
-    var TYPE = 'type'
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+  it('should return error when res is empty', (done) => {
+    const TYPE = 'type';
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
-      callback(null, null)
-    }
+    retryRequestOverride = (reqOpts, opts, callback) => {
+      callback(null, null);
+    };
 
-    getMetadata(function (err) {
-      assert(err instanceof Error)
-      done()
-    })
-  })
+    getMetadata((err) => {
+      assert(err instanceof Error);
+      done();
+    });
+  });
 
-  it('should return error when flavor header is incorrect', function (done) {
-    var TYPE = 'type'
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+  it('should return error when flavor header is incorrect', (done) => {
+    const TYPE = 'type';
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       callback(null, {
         headers: {
           'Metadata-Flavor': 'Hazelnut'
         }
-      })
-    }
+      });
+    };
 
-    getMetadata(function (err) {
-      assert(err instanceof Error)
-      done()
-    })
-  })
+    getMetadata((err) => {
+      assert(err instanceof Error);
+      done();
+    });
+  });
 
-  it('should return error if statusCode is not 200', function (done) {
-    var TYPE = 'type'
-    var getMetadata = gcpMetadata._buildMetadataAccessor(TYPE)
+  it('should return error if statusCode is not 200', (done) => {
+    const TYPE = 'type';
+    const getMetadata = gcpMetadata._buildMetadataAccessor(TYPE);
 
-    retryRequestOverride = function (reqOpts, opts, callback) {
+    retryRequestOverride = (reqOpts, opts, callback) => {
       callback(null, {
         headers: {
           'Metadata-Flavor': 'Google'
         },
         statusCode: 418
-      })
-    }
+      });
+    };
 
-    getMetadata(function (err) {
-      assert(err instanceof Error)
-      done()
-    })
-  })
-})
+    getMetadata((err) => {
+      assert(err instanceof Error);
+      done();
+    });
+  });
+});
