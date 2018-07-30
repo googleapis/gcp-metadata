3ca00c9463262657b766f085a919f2ed0abc9722
Justin Beckwith <justin.beckwith@gmail.com>
2018-07-30 09:33:37 -0700

    chore: make it OSPO compliant

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
 import axios from 'axios';
 import * as rax from 'retry-axios';
 
--- test/fixtures/kitchen/src/index.ts
+++ test/fixtures/kitchen/src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
 import * as gcp from 'gcp-metadata';
 // uncomment the line below during development
 //import * as gcp from '../../../../build/src/index';
