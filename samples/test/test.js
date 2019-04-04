382745ffc3ad2c0e34527093c135663edc9956ff
Jonathan Lui <jonathanlui@google.com>
2019-04-04 16:27:15 -0700

    refactor: wrap execSync with encoding: utf-8

--- samples/test/test.js
+++ samples/test/test.js
@@ -6,7 +6,9 @@
  */
 
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
+
+const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('gcp-metadata samples', () => {
   it('should run the quickstart', async () => {
