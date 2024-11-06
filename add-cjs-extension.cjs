86e48f88f7783c1fceb833d09799d87d86ba54ab
Sofia Leon <sofialeon@google.com>
2024-11-05 16:56:17 -0800

    retry

--- add-cjs-extension.cjs
+++ add-cjs-extension.cjs
@@ -4,14 +4,9 @@ const path = require('path');
 function main(fileNames) {
     for (const file of fileNames) {
         const filePath = path.join(__dirname, file);
-        console.log(filePath)
         let contents = fs.readFileSync(filePath, 'utf8');
-        // console.log(contents)
-        console.log(contents.includes('proxyquire'))
         if (contents.includes('proxyquire')) {
-            contents = contents.replaceAll(/\.js/g, '.cjs');
-            // console.log(contents)
-            console.log(filePath)
+            contents = contents.replace(/\.js/g, '.cjs');
             fs.writeFileSync(filePath, contents);
         }
     };
